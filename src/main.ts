e9211693345f66601648be2e795064c0deec9014
tmothy khalayi <nandwatimothykhalayi@GMAIL.COM>
2025-07-30 11:44:45 +0300

    refactor(main): format import statements for improved readability

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
-import {ValidationPipe,Catch,ExceptionFilter,ArgumentsHost,HttpException,HttpStatus,} from '@nestjs/common';
+import {
+  ValidationPipe,
+  Catch,
+  ExceptionFilter,
+  ArgumentsHost,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { Request, Response } from 'express';
 import { LogsService } from './logs/logs.service';
